e2f4f2bbbf3b03b6a9e14eb0164009855e56cf9a
Damien Sutevski <dameyawn@gmail.com>
2016-02-23 22:41:05 -0800

    Create hash.rb

---
+++ hash.rb
@@ -0,0 +1,16 @@
+class Hash
+  
+  def bury *args
+    puts args
+    if args.count < 2
+      raise ArgumentError.new("2 or more arguments required")
+    elsif args.count == 2
+      self[args[0]] = args[1]
+    else
+      arg = args.shift
+      self[arg] = {} unless self[arg]
+      self[arg].bury(*args) unless args.empty?
+    end
+    self
+  end
+end
